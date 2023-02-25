e0b5729e6cab00ad53fd0e1f4a145e0547b8b679
Anandagadugin <109084312+Anandagadugin@users.noreply.github.com>
2023-02-25 12:44:59 +0530

    changes to make BOLD & COLOUR

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Pushing first code changes')
+// console.log(document.head);
+// console.log(document.domain);
+// console.log(document.images);
+// console.log(documnet.getElementById('header-title'));
+//
+
+items[1].style.fontweight='bold';
+items[1].style.backgroundcolor = 'green';
